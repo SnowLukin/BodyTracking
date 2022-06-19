64fa2713430c4f09d0c9b150b97a83cf79451bcd
Snow Lukin <everyoursden@gmail.com>
2022-06-19 14:37:16 +0300

    Added didUpdate function for arview. Added setup function.

--- BodyTracing/BodyTracing/ARViewContainer.swift
+++ BodyTracing/BodyTracing/ARViewContainer.swift
@@ -9,12 +9,17 @@ import SwiftUI
 import ARKit
 import RealityKit
 
+private var bodySkeleton: BodySkeleton?
+private let bodySkeletonAnchor = AnchorEntity()
+
 struct ARViewContainer: UIViewRepresentable {
     
     typealias UIViewType = ARView
     
     func makeUIView(context: Context) -> ARView {
         let arView = ARView(frame: .zero, cameraMode: .ar, automaticallyConfigureSession: true)
+        arView.setupForBodyTracking()
+        arView.scene.addAnchor(bodySkeletonAnchor)
         
         return arView
     }
@@ -22,6 +27,29 @@ struct ARViewContainer: UIViewRepresentable {
     func updateUIView(_ uiView: ARView, context: Context) {
         
     }
-    
-    
+}
+
+extension ARView: ARSessionDelegate {
+    func setupForBodyTracking() {
+        let configuration = ARBodyTrackingConfiguration()
+        self.session.run(configuration)
+
+        self.session.delegate = self
+    }
+
+    public func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+        for anchor in anchors {
+            if let bodyAnchor = anchor as? ARBodyAnchor {
+                if let skeleton = bodySkeleton {
+                    // BodySkeleton already exists -> update all joints and bones
+                    skeleton.update(with: bodyAnchor)
+                } else {
+                    // BodySkeleton doesnt exist yet -> Body detected for the first time
+                    // Create body skeleton entity and add it to the bodySkeletonAnchor
+                    bodySkeleton = BodySkeleton(for: bodyAnchor)
+                    bodySkeletonAnchor.addChild(bodySkeleton!)
+                }
+            }
+        }
+    }
 }
